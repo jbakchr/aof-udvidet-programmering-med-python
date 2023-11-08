c14b0f6d787adb4c52117008878eb67f8278d25a
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-11-08 20:01:01 +0100

    Changed example 3

--- 01-lectures/01-aften/03-difference-func-and-oop/main.py
+++ 01-lectures/01-aften/03-difference-func-and-oop/main.py
@@ -18,7 +18,7 @@ p2_salary = 50000000000
 def give_a_raise(name, job):
     print(f"Should we give {name} a raise? ", end="")
     if job == "Programming GOD":
-        print("Hell yeah!")
+        print("Hell yeah!", end="\n\n")
     else:
         print("Nope ..", end="\n\n")
 
@@ -30,3 +30,23 @@ def say_hello(name, job, salary):
 
 say_hello(p1_name, p1_job, p1_salary)
 say_hello(p2_name, p2_job, p2_salary)
+
+
+# OBJECT ORIENTED PROGRAMMING
+class Person:
+    def __init__(self, name, job, salary):
+        self.name = name
+        self.job = job
+        self.salary = salary
+
+    def say_hello(self):
+        print(
+            f"Hello! I'm {self.name} and I work as a {self.job} and earn {self.salary} $ a moth"
+        )
+
+
+person1 = Person("Jonas", "Shoe salesman", 50)
+person2 = Person("Niels", "Programmer", 987654321)
+
+person1.say_hello()
+person2.say_hello()
