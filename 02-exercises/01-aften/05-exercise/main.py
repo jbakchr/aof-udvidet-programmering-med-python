ddd1e973bc372b287eb05dba857fbb64ca9b1237
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-11-09 14:00:22 +0100

    Added exercise 5

---
+++ 02-exercises/01-aften/05-exercise/main.py
@@ -0,0 +1,7 @@
+"""
+Exercise 5
+
+Create a simple "Dog" class with "bark" method that just prints something.
+
+Create an instance of your dog class and call the "bark" method.
+"""
---
+++ 02-exercises/01-aften/05-exercise/solution/main.py
@@ -0,0 +1,16 @@
+"""
+Exercise 5
+
+Create a simple "Dog" class with "bark" method that just prints something.
+
+Create an instance of your dog class and call the "bark" method.
+"""
+
+
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+
+d = Dog()
+d.bark()
