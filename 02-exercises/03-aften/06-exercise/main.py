ebafb193516e8e8bf83d2d01d3caab19faf04341
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-11-23 12:59:16 +0100

    Added exercise 6

---
+++ 02-exercises/03-aften/06-exercise/main.py
@@ -0,0 +1,11 @@
+"""
+Exercise 6
+
+Create a function called "first" that add a variable to the global scope.
+
+Create another function called "second" that changes the value of your newly created global variable.
+
+First, call your "first" function and below your function call print out the value of the global variable.
+
+Secondly, call your "second" function and below it print the changed global variable.
+"""
---
+++ 02-exercises/03-aften/06-exercise/solution/main.py
@@ -0,0 +1,28 @@
+"""
+Exercise 6
+
+Create a function called "first" that add a variable to the global scope.
+
+Create another function called "second" that changes the value of your newly created global variable.
+
+First, call your "first" function and below your function call print out the value of the global variable.
+
+Secondly, call your "second" function and below it print the changed global variable.
+"""
+
+
+def first():
+    global num
+    num = 10
+
+
+def second():
+    global num
+    num = 20
+
+
+first()
+print(num)
+
+second()
+print(num)
