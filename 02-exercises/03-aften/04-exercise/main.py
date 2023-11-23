e84c549b8bf8865ba0a998721b4a4190993fee08
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-11-23 08:24:06 +0100

    Added ex

---
+++ 02-exercises/03-aften/04-exercise/main.py
@@ -0,0 +1,16 @@
+"""
+Exercise 8
+
+Create a parent "Animal" class with a "speak" method that print "Wrooaahh!"
+
+Create a child "Tiger" class that only inherits from the "Animal" class.
+
+Create another child class called "Cat" that inherits from the "Animal" class but overrides its "speak" method.
+
+Call the "speak" in the provided loop.
+
+"""
+
+for animal in (Tiger(), Cat()):
+    # call the "speak" method here
+    pass
